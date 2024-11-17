014455073df8ea29f06e4a2575f7270fbed3996d
Bhagya Karunatilleke <haganerox5@gmail.com>
2024-11-17 19:27:17 +0530

    Cleanup the game cards.

--- src/components/game-grid/GameCard.tsx
+++ src/components/game-grid/GameCard.tsx
@@ -13,13 +13,13 @@ const GameCard = ({ game }: Props) => {
     <Card.Root>
       <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <Card.Body>
-        <Heading fontSize='lg'>{game.name}</Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent='space-between' marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize='lg'>{game.name}</Heading>
       </Card.Body>
     </Card.Root>
   );
