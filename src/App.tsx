3bf946ff0eed9b0ecd149e679acaf2232c13b1bf
Bhagya Karunatilleke <haganerox5@gmail.com>
2024-11-17 13:52:43 +0530

    Filter games by platform.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import GenreList from './components/aside/GenreList';
 import { useState } from 'react';
 import { Genre } from './hooks/useGenres';
 import PlatformSelector from './components/PlatformSelector';
+import { Platform } from './hooks/usePlatforms';
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <Grid
@@ -29,8 +33,14 @@ function App() {
         </GridItem>
       </Box>
       <GridItem area={'main'}>
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+        <PlatformSelector
+          onSelectPlatform={setSelectedPlatform}
+          selectedPlatform={selectedPlatform}
+        />
+        <GameGrid
+          selectedGenre={selectedGenre}
+          selectedPlatform={selectedPlatform}
+        />
       </GridItem>
     </Grid>
   );
--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -2,9 +2,14 @@ import { HStack } from '@chakra-ui/react';
 import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from './ui/menu';
 import { Button } from './ui/button';
 import { BsChevronDown } from 'react-icons/bs';
-import usePlatforms from '@/hooks/usePlatforms';
+import usePlatforms, { Platform } from '@/hooks/usePlatforms';
 
-const PlatformSelector = () => {
+interface Props {
+  onSelectPlatform: (platform: Platform | null) => void;
+  selectedPlatform: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
   const { data, error } = usePlatforms();
 
   if (error) return null;
@@ -14,14 +19,21 @@ const PlatformSelector = () => {
       <MenuTrigger asChild>
         <Button variant='subtle' size='sm'>
           <HStack>
-            Platforms
+            {selectedPlatform?.name ?? 'Platforms'}
             <BsChevronDown />
           </HStack>
         </Button>
       </MenuTrigger>
       <MenuContent>
+        <MenuItem key='all' value='all' onClick={() => onSelectPlatform(null)}>
+          All
+        </MenuItem>
         {data?.map((platform) => (
-          <MenuItem key={platform.id} value={platform.slug}>
+          <MenuItem
+            key={platform.id}
+            value={platform.slug}
+            onClick={() => onSelectPlatform(platform)}
+          >
             {platform.name}
           </MenuItem>
         ))}
