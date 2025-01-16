5072699b9f7aa81c64f71b3ecbc79af80648e021
Bhagya Karunatilleke <bhagya.karunatilleke@grubtech.com>
2025-01-16 22:45:03 +0530

    course pt 2 - 19 - build game trailer

---
+++ src/hooks/useGameTrailers.ts
@@ -0,0 +1,16 @@
+import { GameTrailer } from '@/entities/GameTrailer';
+import { APIClient, FetchDataResponse } from '@/services/api-client';
+import { useQuery } from '@tanstack/react-query';
+import ms from 'ms';
+
+const useGameTrailers = (gameId: number) => {
+  const apiClient = new APIClient<GameTrailer>(`/games/${gameId}/movies`);
+
+  return useQuery<FetchDataResponse<GameTrailer>, Error>({
+    queryKey: ['games', gameId, 'movies'],
+    queryFn: apiClient.getAll,
+    staleTime: ms('1h'),
+  });
+};
+
+export default useGameTrailers;
