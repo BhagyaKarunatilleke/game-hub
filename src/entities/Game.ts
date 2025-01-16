173ed4781a5dc748b224f89dd90b42199e10c2c2
Bhagya Karunatilleke <bhagya.karunatilleke@grubtech.com>
2025-01-16 20:23:01 +0530

    course pt 2 - routing - 18 - building game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
 import { Platform } from '@/entities/Platform';
+import { Genre } from './Genre';
+import { Publisher } from './Publisher';
 
 export interface Game {
   id: number;
@@ -6,7 +8,9 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms?: { platform: Platform }[];
+  genres?: Genre[];
   metacritic: number;
   rating_top: number;
   description_raw?: string;
+  publishers?: Publisher[];
 }
