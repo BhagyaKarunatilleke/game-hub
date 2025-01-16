b7e8e055c10cae47cd5fd60a25a2d9fd71fb93a7
Bhagya Karunatilleke <bhagya.karunatilleke@grubtech.com>
2025-01-16 23:50:45 +0530

    course pt 2 - routing - 21 - improving the layout

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,7 +3,14 @@ import GameAttributes from '@/components/GameAttributes';
 import GameScreenshots from '@/components/GameScreenshots';
 import GameTrailer from '@/components/GameTrailer';
 import useGame from '@/hooks/useGame';
-import { Heading, Spinner, Stack } from '@chakra-ui/react';
+import {
+  Box,
+  GridItem,
+  Heading,
+  SimpleGrid,
+  Spinner,
+  Stack,
+} from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 
 const GameDetailPage = () => {
@@ -21,11 +28,19 @@ const GameDetailPage = () => {
 
   return (
     <>
-      <Heading size={'4xl'}>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshots gameId={game.id} />
+      <SimpleGrid columns={{ base: 1, md: 2 }} gap={4}>
+        <GridItem>
+          <Heading size={'4xl'}>{game.name}</Heading>
+          <ExpandableText>{game.description_raw}</ExpandableText>
+          <GameAttributes game={game} />
+        </GridItem>
+        <GridItem>
+          <Box marginBottom={4}>
+            <GameTrailer gameId={game.id} />
+          </Box>
+          <GameScreenshots gameId={game.id} />
+        </GridItem>
+      </SimpleGrid>
     </>
   );
 };
